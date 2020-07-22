d5cbd4c332af74289de5f64ee4abda93fd252035
gaffasaurus <christopher.yao.27@gmail.com>
2020-07-22 15:30:21 -0700

    can now put items in objects

--- Objects/Clickable.js
+++ Objects/Clickable.js
@@ -31,15 +31,18 @@ class Clickable {
   onClick() {
     const { action: { type, ...action } } = this;
     switch (type) {
+      // Displays text in a box
       case 'text': {
         textbox.setText(action.text);
         textbox.setVisible(true);
         break;
       }
+      // Changes the room
       case 'room': {
         currentRoom = action.dest;
         break;
       }
+      // Gives an item and replaces or destroys the Clickable
       case 'item': {
         const icon = new Image();
         icon.src = action.image;
@@ -47,9 +50,15 @@ class Clickable {
         textbox.setVisible(true);
         inventory.addItem(new Item(icon, action.name, action.id, action.reusable));
         const clickables = roomData[currentRoom].clickables;
-        clickables.splice(clickables.indexOf(this), 1);
+        if (action.replace) {
+          replaceInArray(clickables, this, clickableFromJson(action.replace));
+        } else {
+          removeFromArray(clickables, this);
+        }
         break;
       }
+      // Checks if the key is selected, and if so, displays text and replaces
+      // itself with a new Clickable or destroys itself
       case 'locked': {
         if (inventory.selected.length > 0) {
           const selected = inventory.selected[0];
@@ -60,7 +69,11 @@ class Clickable {
               removeFromArray(inventory.items, selected);
             }
             const clickables = roomData[currentRoom].clickables;
-            replaceInArray(clickables, this, clickableFromJson(action.unlocked));
+            if (action.unlocked) {
+              replaceInArray(clickables, this, clickableFromJson(action.unlocked));
+            } else {
+              removeFromArray(clickables, this);
+            }
             removeFromArray(inventory.selected, selected);
           } else {
             textbox.setText([selected.name + " cannot be used on this object!"]);
@@ -72,6 +85,15 @@ class Clickable {
         }
         break;
       }
+      case 'container': {
+        textbox.setText(action.text);
+        textbox.setVisible(true);
+        const icon = new Image();
+        icon.src = action.image;
+        inventory.addItem(new Item(icon, action.name, action.id, action.reusable));
+
+      }
+      // Plays audio
       case 'audio': {
         const audio = new Audio(action.file);
         if (audio.paused) {
